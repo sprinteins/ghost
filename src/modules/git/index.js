457723d597d14e5fd6e0f97fde7974cc1787fcfe
Tamas Rusz <t.rusz@sprinteins.com>
2018-06-15 09:37:30 +0200

    Add core functionality
    Highly optimized for test projects

---
+++ src/modules/git/index.js
@@ -0,0 +1,68 @@
+const {spawn} = window.require('child_process')
+
+export function log(path, doneCB, progressCB){
+  let noOfFiles = 0;
+  let fileMap = {};
+  const cmd = 'git';
+  const cmdArgs = [
+    'log',
+    '--merges',
+    '--numstat',
+    '-m',
+    '--pretty=tformat:',
+    '--grep=from\ bugfix/',
+  ];
+  
+  const gitLog = spawn(cmd, cmdArgs, {cwd: path});
+  
+  gitLog.stdout.on('data', (data) => {
+    let lines = data.toString().split('\n');
+    lines = lines.slice(0,-1);
+    let date;
+    
+    lines.forEach(line => {
+      let stats = line.split('\t');
+      let additions = parseInt(stats[0]);
+      let deletions = parseInt(stats[1]);
+      let file = stats[2];
+      
+      if(!fileMap[file]){
+        fileMap[file] = {
+          file,
+          additions: 0,
+          deletions: 0,
+          changes: 0,
+          commits: 0,
+          date,
+        }
+      }
+      
+      fileMap[file].additions += additions
+      fileMap[file].deletions += deletions
+      fileMap[file].changes += additions + deletions;
+      fileMap[file].commits += 1;
+    });
+    
+    noOfFiles += lines.length;
+    progressCB(noOfFiles);
+    
+  });
+  
+  gitLog.stderr.on('data', (data) => {
+    console.log('stderr: ' + data);
+  });
+  
+  gitLog.on('close', (code) => {
+    // console.log('child process exited with code ' + code);
+    if (code === 0){
+      console.log('child process complete.');
+    } else {
+      console.log('child process exited with code ' + code);
+    }
+    
+    doneCB(fileMap, noOfFiles);
+    
+  });
+  
+  
+}
---
+++ src/views/start/index.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import {log} from '../../modules/git';
+
+const electron = window.require('electron');
+const dialog = electron.remote.dialog;
+
+
+
+export class Start extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.openFolderDialog = this.openFolderDialog.bind(this);
+    this.logDoneCB = this.logDoneCB.bind(this);
+    this.logProgressCB = this.logProgressCB.bind(this);
+    this.sortByCommits =  _ => this.changeSorting(this.state.fileStats, "commits")
+    this.sortByFile =  _ => this.changeSorting(this.state.fileStats, "file")
+
+    this.state = { 
+      noOfFiles:0, 
+      status: 0,
+      fileStats: [],
+      };
+  }
+  
+  render() {
+    
+    let fileTable;
+    if(this.state.fileStats.length > 0){
+      fileTable = <table className="file-table">
+                    <thead>
+                      <tr>
+                        <td onClick={this.sortByFile}>File</td>
+                        <td onClick={this.sortByCommits}>bug fixes</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {
+                      this.state.fileStats.map(stat => (
+                        <tr key={Math.random()}>
+                          <td>{stat.file}</td><td>{stat.commits}</td>
+                        </tr>
+                      ))
+                    }
+                    </tbody>
+                  </table> 
+      
+    }
+    
+    return (
+      <div className="Start">
+        <button onClick={this.openFolderDialog.bind(this)}>Open Repo</button>
+         <div>{this.state.noOfFiles}</div>
+         <div>
+           
+           {fileTable}
+           
+         </div>
+      </div>
+    );
+  }
+  
+  
+  openFolderDialog(){
+    const path = dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']})[0];
+    
+    log(path, this.logDoneCB, this.logProgressCB );
+    return;
+  }
+  
+  logDoneCB(fileMap, noOfFiles){
+    
+    let fileStats = this.convertfileMapToArray(fileMap);
+    
+    this.changeSorting(fileStats, "commits")
+    
+    console.log(fileStats);
+  }
+  
+  logProgressCB(noOfFiles){
+    this.setState({ noOfFiles: noOfFiles });
+  }
+
+  convertfileMapToArray(fileMap){
+    let fileStats = [];
+    for( let key in fileMap){
+      fileStats.push(fileMap[key]);
+    }
+    
+    return fileStats;
+  }
+  
+  
+  
+  changeSorting(fileStats, attribute){
+    this.sortByAttribute(fileStats, attribute);
+    this.setState({status:1,fileStats});
+  }
+  
+  sortByAttribute(array,attribute){
+    array.sort((a,b)=>{
+      if(a[attribute] > b[attribute]){
+        return -1
+      }
+      if(a[attribute] < b[attribute]){
+        return 1
+      }
+      return 0;
+    });
+  }
+  
+}
+
+
+
