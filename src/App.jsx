f7de2923c0f21bbc41fcd7da34314451fcb258cd
Martin Kampa <m.kampa@sprinteins.com>
2019-05-06 15:35:51 +0200

    Fix eslint issues

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Start from './views/start';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to Glow</h1>
+      </header>
+
+      <Start />
+    </div>
+  );
+}
--- test/spec.js
+++ test/spec.js
@@ -1,42 +1,48 @@
-const Application = require('spectron').Application;
+const { Application } = require('spectron');
 const assert = require('assert');
 const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
 const path = require('path');
+const {
+  afterEach,
+  describe,
+  beforeEach,
+  it,
+} = require('mocha');
 
 // process.env.ELECTRON_START_URL = "http://localhost:1234";
 
-describe('Application launch', function() {
-  this.timeout(10000);
+describe('Application launch', () => {
+  let app;
 
-  beforeEach(function() {
-    this.app = new Application({
+  beforeEach(async () => {
+    app = new Application({
       path: electronPath,
 
       // The following line tells spectron to look and use the main.js file
       // and the package.json located 1 level above.
       args: [path.join(__dirname, '..')],
       env: {
-        ELECTRON_START_URL: 'http://localhost:1234'
-      }
+        ELECTRON_START_URL: 'http://localhost:1234',
+      },
     });
-    return this.app.start();
+    await app.start();
   });
 
-  afterEach(function() {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
+  afterEach(async () => {
+    if (app && app.isRunning()) {
+      await app.stop();
     }
   });
 
-  it('shows an initial window', function() {
-    return this.app.client.getWindowCount().then(count => {
-      assert.equal(count, 1);
-      // Please note that getWindowCount() will return 2 if `dev tools` are opened.
-      // assert.equal(count, 2)
-    });
+  it('shows an initial window', async () => {
+    const count = await app.client.getWindowCount();
+    assert.equal(count, 1);
+    // Please note that getWindowCount() will return 2 if `dev tools` are opened.
+    // assert.equal(count, 2)
   });
 
-  it.only('has "Open Repo" button', async function() {
-    await this.app.client.waitForVisible('.repo-button', 5 * 1000);
+  it('has "Open Repo" button', async () => {
+    await app.client.waitForVisible('.repo-button', 5 * 1000);
   });
+
 });
