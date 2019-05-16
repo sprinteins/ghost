4eb10c20583cd2bc50f8b372bc70d2571dbed412
Heiko Lang <>
2019-05-16 11:26:56 +0200

    added checks for undefined files and lines in the git response

--- src/modules/git/index.js
+++ src/modules/git/index.js
@@ -1,32 +1,44 @@
 const { spawn } = window.bridge;
 
 export default function log(path, doneCB, progressCB) {
+
   let noOfFiles = 0;
-  let fileMap = {};
-  const cmd = "git";
+  const fileMap = {};
+  const cmd = 'git';
   const cmdArgs = [
-    "log",
-    "--merges",
-    "--numstat",
-    "-m",
-    "--first-parent",
-    "master",
-    "--pretty=%cD",
-    "--grep=bugfix/"
+    'log',
+    '--merges',
+    '--numstat',
+    '-m',
+    '--first-parent',
+    'master',
+    '--pretty=%cD',
+    '--grep=bugfix/',
   ];
+
+  // git command:
+  // git log --merges --numstat -m --first-parent master --pretty=%cD --grep=bugfix/
 
   const gitLog = spawn(cmd, cmdArgs, { cwd: path });
 
-  gitLog.stdout.on("data", data => {
-    let lines = data.toString().split("\n");
-    lines = lines.slice(0, -1);
+  gitLog.stdout.on('data', (data) => {
+    let lines = data.toString().split('\n');
+
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i] === '') {
+        lines.splice(i, 1);
+      }
+    }
+
     let date;
 
-    lines.forEach(line => {
-      let stats = line.split("\t");
-      let additions = parseInt(stats[0]);
-      let deletions = parseInt(stats[1]);
-      let file = stats[2];
+    lines.forEach((line) => {
+      const stats = line.split('\t');
+      const additions = parseInt(stats[0]);
+      const deletions = parseInt(stats[1]);
+      const file = stats[2];
+
+      if (file === undefined) return;
 
       if (!fileMap[file]) {
         fileMap[file] = {
@@ -35,7 +47,7 @@ export default function log(path, doneCB, progressCB) {
           deletions: 0,
           changes: 0,
           commits: 0,
-          date
+          date,
         };
       }
 
@@ -45,22 +57,23 @@ export default function log(path, doneCB, progressCB) {
       fileMap[file].commits += 1;
     });
 
-    noOfFiles += lines.length;
+    noOfFiles += lines.length / 2;
     progressCB(noOfFiles);
   });
 
-  gitLog.stderr.on("data", data => {
-    console.log("stderr: " + data);
+  gitLog.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
   });
 
-  gitLog.on("close", code => {
+  gitLog.on('close', (code) => {
     // console.log('child process exited with code ' + code);
     if (code === 0) {
-      console.log("child process complete.");
+      console.log('child process complete.');
     } else {
-      console.log("child process exited with code " + code);
+      console.log(`child process exited with code ${code}`);
     }
 
+    console.log('filemap : ', fileMap);
     doneCB(fileMap, noOfFiles);
   });
 }
