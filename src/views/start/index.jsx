ce6a2afaa4950d11cf658d1bbc7b8b5ef3bd6a67
Heiko Lang <>
2019-06-03 10:36:32 +0200

    established filetable reset after new git-repo has been chosen

--- src/views/start/index.jsx
+++ src/views/start/index.jsx
@@ -54,6 +54,8 @@ export default class Start extends Component {
       ele.classList.add('loadingscreen-active');
       ele.classList.remove('loadingscreen-passive');
 
+      this.state.fileStats = {};
+
       log(givenpath, this.logDoneCB, this.logProgressCB, queryParameter);
     }
   }
