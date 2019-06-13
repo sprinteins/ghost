1367ad8ab36bb66b23367d152611f680c0bbe712
L4B0MB4 <37332872+L4B0MB4@users.noreply.github.com>
2019-06-13 21:56:30 +0200

    Added missing parentheses

--- src/views/start/index.jsx
+++ src/views/start/index.jsx
@@ -54,7 +54,7 @@ export default class Start extends Component {
       ele.classList.add('loadingscreen-active');
       ele.classList.remove('loadingscreen-passive');
 
-      this.setState({fileStats:{}}
+      this.setState({fileStats:{}})
 
       log(givenpath, this.logDoneCB, this.logProgressCB, queryParameter);
     }
