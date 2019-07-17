e9c328520e023fa51fe3dfcf3a5d0ea4d5de7bc3
thomasam97 <thomasmaquardt97@gmail.com>
2019-07-17 14:45:13 +0200

    Seperated git, render, calcuation and render

---
+++ src/modules/git/calculations.js
@@ -0,0 +1,29 @@
+export let fileMap = {};
+export let finalcount = 0;
+
+export default function doTheCalculations(newFileMap) {
+  finalcount = newFileMap.length;
+  fileMap = {};
+
+  for (let i = 0; i < finalcount; i += 1) {
+    const { file } = newFileMap[i];
+    if (!fileMap[file]) {
+      fileMap[file] = {
+        file,
+        additions: +newFileMap[i].stats[0],
+        deletions: +newFileMap[i].stats[1],
+        changes: +newFileMap[i].stats[0] + +newFileMap[i].stats[1],
+        commits: 1,
+        latestDate: newFileMap[i].latestDate,
+      };
+    } else {
+      // no comparison of the dates, because the first appearance should be the newest one
+      fileMap[file].additions += +newFileMap[i].stats[0];
+      fileMap[file].deletions += +newFileMap[i].stats[1];
+      fileMap[file].changes += +newFileMap[i].stats[0] + +newFileMap[i].stats[1];
+      fileMap[file].commits += 1;
+    }
+  }
+  console.log('finalcount ', finalcount);
+  console.log('finalised fileMap ', fileMap);
+}
