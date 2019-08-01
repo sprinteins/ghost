ea1bd0dc05f9cff65f428fa9c1d3a9a21d9da1b6
thomasam97 <thomasmaquardt97@gmail.com>
2019-08-01 13:02:29 +0200

    Adds first calculations tests

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,19 @@
+/* eslint-disable func-names */
+// import formatting from '../src/modules/git/formatting';
+
+import doTheCalculations, { fileMap } from '../src/modules/git/calculations';
+import formatting from '../src/modules/git/formatting';
+
 const { Application } = require('spectron');
 const assert = require('assert');
 const electronPath = require('electron'); // Require Electron from the binaries included in node_modules.
 const path = require('path');
 const fakeDialog = require('spectron-fake-dialog');
+const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai');
+
+chai.should();
+chai.use(chaiAsPromised);
 
 // process.env.ELECTRON_START_URL = "http://localhost:1234";
 
@@ -72,3 +83,40 @@ describe('Application launch', function () {
     assert.equal(element, '1 Bugfix_2.txt 2 2019-02-06 T10:15:28');
   });
 });
+
+// non UI tests
+describe('calculations tests', () => {
+  const newFileMap = [{ file: 'Bugfix_2.txt', latestDate: '2019-02-06 T10:15:28', stats: [3, 1, 'Bugfix_2.txt'] },
+  { file: 'Bugfix_2.txt', latestDate: '2019-02-06 T10:04:11', stats: [5, 0, 'Bugfix_2.txt'] },
+  { file: 'Bugfix_1.txt', latestDate: '2019-02-06 T10:00:42', stats: [3, 1, 'Bugfix_1.txt'] }];
+  doTheCalculations(newFileMap);
+
+  it('amout of additions should be calculated as 8 and 3 and filled into the fileMap', () => {
+    const isValid = fileMap['Bugfix_2.txt'].additions;
+    assert.equal(isValid, 8);
+    const secondValidation = fileMap['Bugfix_1.txt'].additions;
+    assert.equal(secondValidation, 3);
+  });
+
+  it('amout of deletions should be calculated as 1 and 1 and filled into the fileMap', () => {
+    const isValid = fileMap['Bugfix_2.txt'].deletions;
+    assert.equal(isValid, 1);
+    const secondValidation = fileMap['Bugfix_1.txt'].deletions;
+    assert.equal(secondValidation, 1);
+  });
+
+  it('amout of changes should be calculated as 4 and 9 and filled into the fileMap', () => {
+    const isValid = fileMap['Bugfix_1.txt'].changes;
+    assert.equal(isValid, 4);
+    const secondValidation = fileMap['Bugfix_2.txt'].changes;
+    assert.equal(secondValidation, 9);
+  });
+
+  it('amout of commits should be calculated as 2 and 1 and filled into the fileMap', () => {
+    const isValid = fileMap['Bugfix_2.txt'].commits;
+    assert.equal(isValid, 2);
+    const secondValidation = fileMap['Bugfix_1.txt'].commits;
+    assert.equal(secondValidation, 1);
+  });
+});
+
