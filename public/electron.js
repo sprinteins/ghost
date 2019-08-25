dfa1c3edc3b02d2f2cec40ed2886e4be15164afd
L4B0MB4 <metalfan65@gmail.com>
2019-08-25 21:43:10 +0200

    remove electron.js that required electron

--- public/electron.js
+++
@@ -1,6 +0,0 @@
-/**
- * I dont like to place my main.js for electron here - so I´m just requiring it.
- * Maybe there´s a better way but I guess one had to eject react
- */
-
-require("./backend/main");
